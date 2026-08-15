49c5bc019b58dd1e42b4b14f8ebc79d86eb24d0f
Dicklesworthstone <jeff141421@gmail.com>
2026-08-14 20:34:48 -0400

    test(graphical): preserve complement isolated degrees (br-r37-c1-8aati)

--- tests/python/test_large_graph_numerical_parity.py
+++ tests/python/test_large_graph_numerical_parity.py
@@ -638,6 +638,13 @@ def test_complement_preserves_isolated_nodes():
     complement = fnx.complement(graph)
     assert "isolated" in complement
 
+
+def test_complement_isolated_node_has_full_degree():
+    graph = fnx.Graph()
+    graph.add_nodes_from([0, 1, 2, 3])
+    complement = fnx.complement(graph)
+    assert complement.degree(0) == len(graph) - 1
+
     directed = fnx.DiGraph()
     directed.add_edges_from([(0, 1), (1, 3), (0, 2), (2, 3), (1, 2)])
     directed_mapping = {node: f"directed-path-{node}" for node in directed}
